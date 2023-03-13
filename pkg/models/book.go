47f61926e10e5aca4d2a0eba22af804c7665387a
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-03-13 19:25:40 +0545

    setting up models file

---
+++ pkg/models/book.go
@@ -0,0 +1,21 @@
+package models
+
+import (
+	"github.com/bicky-tmg/go-bookstore/pkg/config"
+	"github.com/jinzhu/gorm"
+)
+
+var db *gorm.DB
+
+type Book struct {
+	gorm.Model
+	Name        string `gorm:""json:"name"`
+	Author      string `json:"author"`
+	Publication string `json:"publication"`
+}
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&Book{})
+}
