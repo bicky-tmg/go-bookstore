a746047b2a47ffb8274f2810c0a0da77677527d7
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-03-15 20:50:59 +0545

    defining book models file

--- pkg/models/book.go
+++ pkg/models/book.go
@@ -19,3 +19,31 @@ func init() {
 	db = config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book) CreateBook() *Book {
+	db.NewRecord(b)
+	db.Create(&b)
+
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db := db.Where("ID=?", Id).Find(&getBook)
+
+	return &getBook, db
+}
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.Where("ID=?", ID).Delete(book)
+
+	return book
+}
